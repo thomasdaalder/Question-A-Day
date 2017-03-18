66526bdc2c01a78aa41d187a9dd28e3e2c5ba0e3
Thomas Daalder <thomasdaalder@gmail.com>
2017-03-18 16:59:50 +0100

    added jquery on splash page

---
+++ public/js/custom.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    Typed.new("#typed", {
+        stringsElement: document.getElementById('typed-strings'),
+        typeSpeed: 30,
+        backDelay: 500,
+        loop: false,
+        contentType: 'html', // or text
+        // defaults to null for infinite loop
+        loopCount: null,
+        callback: function(){ foo(); },
+        resetCallback: function() { newTyped(); }
+    });
+
+    var resetElement = document.querySelector('.reset');
+    if(resetElement) {
+        resetElement.addEventListener('click', function() {
+            document.getElementById('typed')._typed.reset();
+        });
+    }
+
+});
+
+function newTyped(){ /* A new typed object */ }
+
+function foo(){ console.log("Callback"); }
